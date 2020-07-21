5e23ac579e732b2d11e48c6af08b8265704ca7eb
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-07-21 23:05:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,12 +6,11 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    item_name = req.params["q"]
-
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
 
       item = @@items.find{|i| i.name == item_name}
+
       if item.nil?
         resp.write "Item not found"
         resp.status = 400
@@ -22,5 +21,7 @@ class Application
       resp.write "Route not found"
       resp.status = 404
     end
+
+    resp.finish
   end
 end
